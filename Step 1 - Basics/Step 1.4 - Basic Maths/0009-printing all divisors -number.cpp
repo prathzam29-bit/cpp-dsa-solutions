1a57375d8d9deacac62ea49d4580d95d2b302f2c
prathzam29-bit <prathzam29@gmail.com>
2026-08-16 17:58:56 +0530

    Add printing divisors solution from tuf

---
+++ Step 1 - Basics/Step 1.4 - Basic Maths/0009-printing all divisors -number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> divisors(int n) {
+        vector<int> v;
+        
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                v.push_back(i);
+                
+                if ((n / i) != i) {
+                    v.push_back(n / i);
+                }
+            }
+        }
+        
+        sort(v.begin(), v.end());
+        return v;
+    }
+}; // converting big o (n) to big o (root n)
